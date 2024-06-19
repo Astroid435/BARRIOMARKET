31777ef7aaf2549004a32f0936e7007c662e18c3
alan-sg17 <alanscg10@gmail.com>
2024-06-19 10:57:06 -0500

    Vista producto

--- proyecto/vista_producto.php
+++ proyecto/vista_producto.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-<meta charset="UTF-8">
+  <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Barrio Market</title>
   <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
@@ -10,9 +11,34 @@
   <script src="js/bootstrap.min.js"></script>
   <link rel="stylesheet" type="text/css" href="style/estilos_dashboard.css">
   <meta charset="utf-8">
+  <style>
+    .container-info {
+      margin-top: 35px;
+    }
+
+    #precio {
+      font-size: 25px;
+      font-weight: 450;
+    }
+
+    h2 {
+      margin-bottom: 20px;
+    }
+
+    .inicio-log {
+      color: white;
+      background-color: black;
+      width: 100%;
+      height: 42px;
+      border-radius: 10px;
+      font-size: large;
+      border: 1px black solid !important;
+    }
+  </style>
 </head>
+
 <body>
-<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+  <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
     <symbol id="bootstrap" viewBox="0 0 118 94">
       <title>Bootstrap</title>
       <path fill-rule="evenodd" clip-rule="evenodd" d="M24.509 0c-6.733 0-11.715 5.893-11.492 12.284.214 6.14-.064 14.092-2.066 20.577C8.943 39.365 5.547 43.485 0 44.014v5.972c5.547.529 8.943 4.649 10.951 11.153 2.002 6.485 2.28 14.437 2.066 20.577C12.794 88.106 17.776 94 24.51 94H93.5c6.733 0 11.714-5.893 11.491-12.284-.214-6.14.064-14.092 2.066-20.577 2.009-6.504 5.396-10.624 10.943-11.153v-5.972c-5.547-.529-8.934-4.649-10.943-11.153-2.002-6.484-2.28-14.437-2.066-20.577C105.214 5.894 100.233 0 93.5 0H24.508zM80 57.863C80 66.663 73.436 72 62.543 72H44a2 2 0 01-2-2V24a2 2 0 012-2h18.437c9.083 0 15.044 4.92 15.044 12.474 0 5.302-4.01 10.049-9.119 10.88v.277C75.317 46.394 80 51.21 80 57.863zM60.521 28.34H49.948v14.934h8.905c6.884 0 10.68-2.772 10.68-7.727 0-4.643-3.264-7.207-9.012-7.207zM49.948 49.2v16.458H60.91c7.167 0 10.964-2.876 10.964-8.281 0-5.406-3.903-8.178-11.425-8.178H49.948z"></path>
@@ -82,12 +108,57 @@
 
   <div class="container">
     <div class="row">
-      <img src="https://http2.mlstatic.com/caja-de-lapiceros-bic-cristal-negro-y-azul-D_NQ_NP_679117-MLV27338303248_052018-F.jpg" class="col-md-7" height="800px" width="10%">
+      <img src="https://http2.mlstatic.com/caja-de-lapiceros-bic-cristal-negro-y-azul-D_NQ_NP_679117-MLV27338303248_052018-F.jpg" class="col-md-7" height="700px" width="200px">
       <div class="container-info col-md-5">
         <h2><strong>12 Boligrafos Lapicero</strong></h2>
+        <p>Descripcion del Producto</p>
+        <ul>
+          <li style="color: black !important;">Lapicero</li>
+          <li>Tinta semigel</li>
+          <li>Cuerpo triangular</li>
+          <li>Suavidad incomparable</li>
+          <LI>Color vivo y intenso</LI>
+        </ul>
+        <p id="precio"><strong>$10.10</strong></p>
+        <p>Para agregar el producto al carrito inicie sesión</p>
+        <button type="submit" class="inicio-log">Iniciar sesión</button>
+
+      </div>
+      <div class="row">
+        <h2>Productos relacionados</h2>
+      <div class="row">
+        <div class="producto1 col-md-4">
+          <img src="https://s3.amazonaws.com/imagenes-sellers-mercado-ripley/2020/03/04165305/343211.jpg" height="200px" width="100%">
+          <div class="container-info">
+            <p id="nom_produ"><strong>producto</strong></p>
+            <p id="info_produ">descripcion del producto</p>
+            <p id="info_produ"><strong>$10.99</strong></p>
+          </div>
+        </div>
+
+        <div class="producto1 col-md-4">
+          <img src="https://s3.amazonaws.com/imagenes-sellers-mercado-ripley/2020/03/04165305/343211.jpg" height="200px" width="100%">
+          <div class="container-info">
+            <p id="nom_produ"><strong>producto</strong></p>
+            <p id="info_produ">descripcion del producto</p>
+            <p id="info_produ"><strong>$10.99</strong></p>
+          </div>
+        </div>
+
+        <div class="producto1 col-md-4">
+          <img src="https://s3.amazonaws.com/imagenes-sellers-mercado-ripley/2020/03/04165305/343211.jpg" height="200px" width="100%">
+          <div class="container-info">
+            <p id="nom_produ"><strong>producto</strong></p>
+            <p id="info_produ">descripcion del producto</p>
+            <p id="info_produ"><strong>$10.99</strong></p>
+          </div>
+        </div>
+        
+      </div>
       </div>
     </div>
 
   </div>
 </body>
+
 </html>
